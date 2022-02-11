a17a543893c14bd63c642c22fd96d43abe9b9e3c
João Xavier <jvxavier@inf.ufsm.br>
2022-02-11 19:29:29 -0300

    tirando a url do banco de dados

--- back-end/src/Database/connection.js
+++ back-end/src/Database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const url = "mongodb+srv://root:8HxRTl2N8P3nb4IS@todolist.0fvwx.mongodb.net/ToDoList?retryWrites=true&w=majority"
+const url = "Place the URL connection data base here"
 
 mongoose.connect(url);
 mongoose.Promise = global.Promise;
